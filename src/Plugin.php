e4bd7a41b521f9e254dae1d1754422368e9c4e6f
kriss <462679766@qq.com>
2020-11-15 22:18:09 +0800

    feat✨ support composer2

--- src/Plugin.php
+++ src/Plugin.php
@@ -31,6 +31,14 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         $this->io = $io;
     }
 
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
     public static function getSubscribedEvents()
     {
         return array(
